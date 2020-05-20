a32f528de33fbb258b7157d0218cd405d36736e7
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-20 13:49:30 -0400

    Notification controller methodes

--- src/main/java/ca/usherbrooke/notifius/restcontrollers/NotificationController.java
+++ src/main/java/ca/usherbrooke/notifius/restcontrollers/NotificationController.java
@@ -1,5 +1,42 @@
 package ca.usherbrooke.notifius.restcontrollers;
 
+import ca.usherbrooke.notifius.models.Notification;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class NotificationController
 {
+    @GetMapping("/users/{userId}/notifications")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Notification> getNotifications(@PathVariable("userId") String userId)
+    {
+        return null;
+    }
+
+    @PostMapping("/users/{userId}/notifications")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Notification createNotificationByUser(@PathVariable("userId") String userId)
+    {
+        return null;
+    }
+
+    @PostMapping("/trimester/{trimesterId}/activities/{activityId}/users/notifications")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Notification createNotificationsByActivity(@PathVariable("trimesterId") String trimesterId,
+                                                      @PathVariable("activityId") String activityId)
+    {
+        return null;
+    }
+
+    @PostMapping("/departments/{departmentId}/sessions/{sessionId}/trimesters/{trimesterId}/users/notifications")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Notification createNotificationsByDepartment(@PathVariable("departmentId") String departmentId,
+                                                        @PathVariable("sessionId") String sessionId,
+                                                        @PathVariable("trimesterId") String trimesterId)
+    {
+        return null;
+    }
 }
--- src/main/java/ca/usherbrooke/notifius/restcontrollers/SettingsController.java
+++ src/main/java/ca/usherbrooke/notifius/restcontrollers/SettingsController.java
@@ -1,5 +1,10 @@
 package ca.usherbrooke.notifius.restcontrollers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SettingsController
 {
+
 }
