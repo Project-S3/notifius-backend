54a1572288ee7d1f2c8bfbea20b263d7eaf73dc4
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-20 13:30:04 -0400

    NotifiusApplication

---
+++ src/main/java/ca/usherbrooke/notifius/NotifiusApplication.java
@@ -0,0 +1,13 @@
+package ca.usherbrooke.notifius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifiusApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NotifiusApplication.class, args);
+    }
+}
