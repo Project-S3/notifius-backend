b355b2dd5f5c77b0aa57ed653e7ac858a6a19bdf
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-20 15:27:59 -0400

    max-string-length in notification service

--- src/main/java/ca/usherbrooke/notifius/validators/NotificationValidator.java
+++ src/main/java/ca/usherbrooke/notifius/validators/NotificationValidator.java
@@ -1,11 +1,15 @@
 package ca.usherbrooke.notifius.validators;
 
 import ca.usherbrooke.notifius.models.Notification;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationValidator
 {
+    @Value("${notifius.request.max-string-length}")
+    private Integer maxStringLength;
+
     public boolean validNotification(Notification notification)
     {
         return true;
