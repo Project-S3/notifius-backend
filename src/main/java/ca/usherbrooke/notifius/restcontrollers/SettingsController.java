3f2fc498f0bf1b5ae8b63f4738df6d6193a2207a
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-20 13:52:20 -0400

    Settingsn controller methodes

--- src/main/java/ca/usherbrooke/notifius/restcontrollers/SettingsController.java
+++ src/main/java/ca/usherbrooke/notifius/restcontrollers/SettingsController.java
@@ -1,10 +1,23 @@
 package ca.usherbrooke.notifius.restcontrollers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ca.usherbrooke.notifius.models.Settings;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SettingsController
 {
+    @GetMapping("/users/{userID}/settings")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Settings getSettingByUser(@PathVariable("userID") String userID)
+    {
+        return null;
+    }
 
+    @PostMapping("/users/{userID}/settings")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Settings setSettingByUser(@PathVariable("userID") String userID)
+    {
+        return null;
+    }
 }
