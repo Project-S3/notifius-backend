67f1f85c858a01914840417b7525295c4a214fea
Jean-Philippe Lemay <jeanplemay@gmail.com>
2020-05-20 16:05:40 -0400

    NotificationValidator

--- src/main/java/ca/usherbrooke/notifius/validators/NotificationValidator.java
+++ src/main/java/ca/usherbrooke/notifius/validators/NotificationValidator.java
@@ -4,7 +4,13 @@ import ca.usherbrooke.notifius.models.Notification;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Date;
+
+
+
 
 @Service
 public class NotificationValidator
@@ -14,9 +20,21 @@ public class NotificationValidator
 
     public boolean validNotification(Notification notification)
     {
-        if(notification.getContent().length() > maxStringLength) return false;
-        if(notification.getTitle().length() > maxStringLength) return false;
-        if(notification.getDate().compareTo(new Date()) > 0 ) return false;
+        if(notification.getContent().length() > maxStringLength)
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "Le contenu est trop long.");
+
+        if(notification.getContent().isEmpty())
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_REQUIRED, "Vous devez fournir un contenu.");
+
+        if(notification.getTitle().length() > maxStringLength)
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "Le titre est trop long.");
+
+        if(notification.getContent().isEmpty())
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_REQUIRED, "Vous devez fournir un titre.");
+
+        if(notification.getDate().compareTo(new Date()) > 0 )
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED, "La date est invalide.");
+
         return true;
     }
 }
