49720eda3233f23726cd10b76a37f28b81c2d11e
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-20 17:08:22 -0400

    translator

--- src/main/java/ca/usherbrooke/notifius/translators/NotificationToEntityTranslator.java
+++ src/main/java/ca/usherbrooke/notifius/translators/NotificationToEntityTranslator.java
@@ -1,5 +1,16 @@
 package ca.usherbrooke.notifius.translators;
 
+import ca.usherbrooke.notifius.entities.NotificationEntity;
+import ca.usherbrooke.notifius.models.Notification;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationToEntityTranslator
 {
+    public NotificationEntity toEntity(Notification notification)
+    {
+        return new NotificationEntity().withTitle(notification.getTitle())
+                                       .withContent(notification.getTitle())
+                                       .withDate(notification.getDate());
+    }
 }
