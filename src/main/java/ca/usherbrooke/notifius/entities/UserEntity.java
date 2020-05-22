c3eef6068a8b6a1307dc034fd052ce6169bacd16
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-05-22 13:26:25 -0400

    user entity

--- src/main/java/ca/usherbrooke/notifius/entities/UserEntity.java
+++ src/main/java/ca/usherbrooke/notifius/entities/UserEntity.java
@@ -8,18 +8,19 @@ import java.util.Set;
 public class UserEntity
 {
     @Id
-    @Column(unique = true)
     private String id;
+    @Column
+    private String email;
+    @Column
+    private String phoneNumber;
 
     @OneToOne(mappedBy = "user")
-    private SettingEntity setting;
-
+    private SettingEntity settings;
     @OneToMany(mappedBy = "user")
     private Set<NotificationEntity> notifications;
 
     public UserEntity()
     {
-
     }
 
     public UserEntity(String id)
@@ -40,6 +41,69 @@ public class UserEntity
     public UserEntity withId(String id)
     {
         setId(id);
+        return this;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public UserEntity withEmail(String email)
+    {
+        setEmail(email);
+        return this;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserEntity withPhoneNumber(String phoneNumber)
+    {
+        setPhoneNumber(phoneNumber);
+        return this;
+    }
+
+    public SettingEntity getSettings()
+    {
+        return settings;
+    }
+
+    public void setSettings(SettingEntity settings)
+    {
+        this.settings = settings;
+    }
+
+    public UserEntity withSettings(SettingEntity settings)
+    {
+        setSettings(settings);
+        return this;
+    }
+
+    public Set<NotificationEntity> getNotifications()
+    {
+        return notifications;
+    }
+
+    public void setNotifications(Set<NotificationEntity> notifications)
+    {
+        this.notifications = notifications;
+    }
+
+    public UserEntity withNotifications(Set<NotificationEntity> notifications) {
+        setNotifications(notifications);
         return this;
     }
 }
