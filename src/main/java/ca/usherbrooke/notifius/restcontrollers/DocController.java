e34de05cc3a3a0847ef0254a9c2e96e5130a2c75
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-11 09:33:28 -0400

    notifius doc value

--- src/main/java/ca/usherbrooke/notifius/restcontrollers/DocController.java
+++ src/main/java/ca/usherbrooke/notifius/restcontrollers/DocController.java
@@ -1,5 +1,6 @@
 package ca.usherbrooke.notifius.restcontrollers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,14 @@ import java.net.URI;
 @RestController
 public class DocController
 {
+    @Value("${notifius.doc-url}")
+    private String notifiusDocURL;
+
     @GetMapping(path = "")
     public ResponseEntity<Void> redirect()
     {
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("https://documenter.getpostman.com/view/11266465/SzmmTEBG?version=latest"));
+        headers.setLocation(URI.create(notifiusDocURL));
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 }
