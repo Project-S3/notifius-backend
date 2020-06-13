055132456cfe29c9ce8c0ce4d225a6e53af4f645
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-12 20:26:53 -0400

    pas de cas dans le backend

--- src/main/java/ca/usherbrooke/notifius/backend/NotifiusBackendApplication.java
+++ src/main/java/ca/usherbrooke/notifius/backend/NotifiusBackendApplication.java
@@ -1,12 +1,10 @@
 package ca.usherbrooke.notifius.backend;
 
-import org.jasig.cas.client.boot.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableCasClient
 @SpringBootApplication
 public class NotifiusBackendApplication
 {
