b38fcd76b4c472e71470e4c3862f3b977ec8d227
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-18 14:11:56 -0400

    briser pas pour moi

--- src/main/java/ca/usherbrooke/notifius/backend/services/notificationsender/EmailNotificationSender.java
+++ src/main/java/ca/usherbrooke/notifius/backend/services/notificationsender/EmailNotificationSender.java
@@ -2,13 +2,10 @@ package ca.usherbrooke.notifius.backend.services.notificationsender;
 
 import ca.usherbrooke.notifius.backend.models.Notification;
 import ca.usherbrooke.notifius.backend.models.User;
-import ca.usherbrooke.notifius.backend.repositories.NotificationSenderRepository;
 import ca.usherbrooke.notifius.backend.services.EmailService;
 import ca.usherbrooke.notifius.backend.services.UserNotificationSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 public class EmailNotificationSender extends NotificationSender
