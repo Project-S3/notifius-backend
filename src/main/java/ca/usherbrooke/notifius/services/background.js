e4cbac21e3b5683d35a648bd153bf11f53d7e179
JuanManuelGallego <47032262+JuanManuelGallego@users.noreply.github.com>
2020-06-24 12:41:44 -0400

    Chrome Extension 2.0

--- src/main/java/ca/usherbrooke/notifius/services/background.js
+++ src/main/java/ca/usherbrooke/notifius/services/background.js
@@ -1,7 +1,4 @@
-chrome.runtime.onInstalled.addListener(function(object){
-    console.log("Extension and content scripts installed");
-    chrome.tabs.create({url: "https://www.gel.usherbrooke.ca/s3i/e20/"}, function (tab) {
-        console.log("New tab launched with http://yoursite.com/");
-    });
+chrome.runtime.onInstalled.addListener(function (object) {
+	//Ouvrir une page web lors de l'installation de l'extension
+	if(object.reason === 'install')chrome.tabs.create({url: "https://www.gel.usherbrooke.ca/s3i/e20/"}, function (tab) {});
 });
-
--- src/main/java/ca/usherbrooke/notifius/services/popup.js
+++ src/main/java/ca/usherbrooke/notifius/services/popup.js
@@ -1,15 +1,16 @@
-//function notify(message) {
+notify("Nouvelle note")
+function notify(message) {
     var Url = 'https://www.gel.usherbrooke.ca/s3i/e20/';
     var notif = {
         type: 'basic',
         iconUrl: 'Notifus_Logo.png',
         title: 'Notifus',
-        message: 'Tu as eu 49% a ton examen..'
+        message: message
     };
-console.log("Extension and content scripts installed");
+
     chrome.notifications.create(Url, notif);
 
     chrome.notifications.onClicked.addListener(function (notificationId, byUser) {
         chrome.tabs.create({url: Url});
     });
-//}
+}
