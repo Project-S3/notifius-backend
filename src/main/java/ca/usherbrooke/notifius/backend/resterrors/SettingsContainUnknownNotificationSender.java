6b6ae78e93948667395e1025c2cc4d478d971263
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-07-09 10:17:49 -0400

    valid value

--- src/main/java/ca/usherbrooke/notifius/backend/resterrors/SettingsContainUnknownNotificationSender.java
+++ src/main/java/ca/usherbrooke/notifius/backend/resterrors/SettingsContainUnknownNotificationSender.java
@@ -9,10 +9,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class SettingsContainUnknownNotificationSender extends ResponseStatusException
 {
-
     public SettingsContainUnknownNotificationSender()
     {
         super(HttpStatus.PRECONDITION_FAILED, "Il y a un type de notification sender qui n'existe pas dans les settingds");
     }
-
 }
