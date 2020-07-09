c39a27021c382217e548dde33453a0a84773c003
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-07-09 09:36:06 -0400

    test notification validator

--- src/test/java/ca/usherbrooke/notifius/backend/validators/NotificationValidatorTest.java
+++ src/test/java/ca/usherbrooke/notifius/backend/validators/NotificationValidatorTest.java
@@ -5,15 +5,20 @@
 package ca.usherbrooke.notifius.backend.validators;
 
 import ca.usherbrooke.notifius.backend.models.Notification;
+import ca.usherbrooke.notifius.backend.resterrors.ContentTooLongException;
+import ca.usherbrooke.notifius.backend.resterrors.DateInvalidException;
 import ca.usherbrooke.notifius.backend.resterrors.TitleTooLongException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static ca.usherbrooke.notifius.backend.models.Service.TEST;
+import static java.util.Calendar.HOUR_OF_DAY;
 
 @RunWith(SpringRunner.class)
 public class NotificationValidatorTest
@@ -38,5 +43,21 @@ public class NotificationValidatorTest
     public void testNotificationTitleTooLong()
     {
         notificationValidator.validNotificationThrowIfNotValid(notification.withTitle("Very long title bla bla bla"));
+    }
+
+    @Test(expected = ContentTooLongException.class)
+    public void testNotificationContentTooLong()
+    {
+        notificationValidator.validNotificationThrowIfNotValid(
+                notification.withContent("A very long content Muda! Muda! Muda! Muda! Muda! Muda! "));
+    }
+
+    @Test(expected = DateInvalidException.class)
+    public void testNotificationDateInvalid()
+    {
+        Calendar c = new GregorianCalendar();
+        c.setTime(new Date());
+        c.add(HOUR_OF_DAY, 1);
+        notificationValidator.validNotificationThrowIfNotValid(notification.withDate(c.getTime()));
     }
 }
